7bc7de93597e959bf6fb8a6567e9773f3b2d8aec
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-14 00:07:59 +0200

    update index.js and router.js

--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -1,9 +1,13 @@
 const addUserQuery = require('./addUserQuery');
 const checkEmailQuery = require('./checkEmailQuery');
 const checkUserQuery = require('./checkUserQuery');
+const insertPostQuery = require('./posts/insertPostQuery');
+const deletePostQuery = require('./posts/deletePostQuery');
 
 module.exports = {
   addUserQuery,
   checkEmailQuery,
   checkUserQuery,
+  insertPostQuery,
+  deletePostQuery,
 };
