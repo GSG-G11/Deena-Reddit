083f5434787408249d6533245bb8182393429160
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-13 23:58:39 +0200

    add post schema

--- server/validation/index.js
+++ server/validation/index.js
@@ -1,7 +1,9 @@
 const signUpSchema = require('./signUpSchema');
 const loginSchema = require('./loginSchema');
+const addPostSchema = require('./addPostSchema');
 
 module.exports = {
   signUpSchema,
   loginSchema,
+  addPostSchema,
 };
