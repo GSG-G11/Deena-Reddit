ce244202334b3c0954724d86aa6d55237bf9e0e8
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-13 17:11:39 +0200

    create check user query

--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -1,7 +1,9 @@
 const addUserQuery = require('./addUserQuery');
 const checkEmailQuery = require('./checkEmailQuery');
+const checkUserQuery = require('./checkUserQuery');
 
 module.exports = {
   addUserQuery,
   checkEmailQuery,
+  checkUserQuery,
 };
