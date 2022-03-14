401c42c62d6bf4e3a9df6b90159b69415e678317
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-14 22:33:41 +0200

    create login in front

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('.login');
+const emailInput = document.querySelector('#email');
+const passInput = document.querySelector('#password');
+const errorMsg = document.querySelector('.errorMsg');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch('/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      email: emailInput.value,
+      password: passInput.value,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.message) {
+        window.location = '/';
+      } else {
+        errorMsg.textContent = data;
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+});
