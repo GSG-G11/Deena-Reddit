542288e6f54321f5b26dd8e97bf307e8b20671da
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-16 00:43:20 +0200

    delete communities from query

--- server/database/queries/posts/insertPostQuery.js
+++ server/database/queries/posts/insertPostQuery.js
@@ -1,6 +1,6 @@
 const connection = require('../../config/connection');
 
 // eslint-disable-next-line camelcase
-const insertPostQuery = (title, post, communities, user_id) => connection.query('INSERT INTO posts (title, post, communities, user_id) VALUES ($1, $2, $3, $4) RETURNING *', [title, post, communities, user_id]);
+const insertPostQuery = (title, post, user_id) => connection.query('INSERT INTO posts (title, post, user_id) VALUES ($1, $2, $3) RETURNING *', [title, post, user_id]);
 
 module.exports = insertPostQuery;
