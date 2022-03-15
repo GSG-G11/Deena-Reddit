423f67a1cf232783b722eca00b4ff92b53956004
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-16 00:33:53 +0200

    edit router.js

--- server/router/router.js
+++ server/router/router.js
@@ -7,15 +7,27 @@ const {
   addPost,
   getPost,
   deletePost,
+  getAllPosts,
+  getAllPostsById,
+  getProfile,
+  increaseVote,
+  decreaseVote,
   clientError,
   serverError,
 } = require('../controller');
 const auth = require('../middleware/auth');
+const userData = require('../middleware/userData');
 
 router.route('/signUp').get(getSignUp).post(signUp);
 router.route('/login').get(auth, getLogin).post(login);
-router.route('/reddit').get(auth, getPost).post(auth, addPost);
-router.route('/reddit/:id').delete(auth, deletePost);
+router.route('/api/v1/allPosts').get(getAllPosts);
+router.route('/api/v1/getAllPostsById/:id').get(getAllPostsById);
+router.route('/addPost').get(auth, getPost).post(auth, addPost);
+router.route('/profile/:id').get(getProfile);
+router.route('/increaseVote/:id').get(auth, increaseVote);
+router.route('/decreaseVote/:id').get(decreaseVote);
+router.route('/userData').get(userData);
+router.route('/post/:id').delete(auth, deletePost);
 router.use(clientError);
 router.use(serverError);
 
