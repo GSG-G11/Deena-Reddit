5a56cda4d7ff50520fc8b02b292f4fd88c936851
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-16 00:14:28 +0200

    add user data

---
+++ server/middleware/userData.js
@@ -0,0 +1,15 @@
+const { verify } = require('jsonwebtoken');
+
+const userData = (request, response) => {
+  if (request.cookies.token) {
+    const { token } = request.cookies;
+
+    return verify(token, process.env.SECRET_KEY, (err, verifyCookies) => {
+      if (err) return response.redirect('/');
+      request.id = verifyCookies.id;
+      response.json({ id: verifyCookies.id });
+    });
+  }
+};
+
+module.exports = userData;
