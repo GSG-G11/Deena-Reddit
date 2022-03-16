b4dd4a92b145650ea4466cee85bb861bffefb528
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-16 16:14:02 +0200

    remove return

--- server/controller/posts/getAllPostsById.js
+++ server/controller/posts/getAllPostsById.js
@@ -2,8 +2,8 @@ const { displayPostsById } = require('../../database/queries');
 
 const getAllPostsById = (req, res, next) => {
   const { id } = req.params;
-  return displayPostsById(id).then((data) => res.status(200).json(data.rows))
-  // eslint-disable-next-line consistent-return
+  displayPostsById(id).then((data) => res.status(200).json(data.rows))
+    // eslint-disable-next-line consistent-return
     .catch((error) => {
       if (error.status) return res.status(error.status).json(error.message);
       next();
