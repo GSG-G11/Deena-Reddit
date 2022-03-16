4fdfe94c93aef233be87e3ff3e477fbdcfca9998
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-16 16:12:45 +0200

    add increse and decrease backend

--- server/controller/posts/decreaseVote.js
+++ server/controller/posts/decreaseVote.js
@@ -3,11 +3,12 @@ const { voteDecrease } = require('../../database/queries');
 const decreaseVote = (req, res, next) => {
   // if (!req.id) return res.status(401).json({ err: 'unauthorized' });
   const { id } = req.params;
-  return voteDecrease(id).then(() => res.status(200).json({ msg: 'vote Decreasing Successfully' }))
+  return voteDecrease(id).then((data) => res.status(200).json(data.rows[0]))
   // eslint-disable-next-line consistent-return
     .catch((error) => {
-      if (error.status) return res.status(error.status).json(error.message);
-      next();
+      console.log(error);
+      // if (error.status) return res.status(error.status).json(error.message);
+      // next();
     });
 };
 
