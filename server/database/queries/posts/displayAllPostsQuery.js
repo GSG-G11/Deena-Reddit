856daaf26af0200e13c10cf15af1086b7612e77d
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-17 15:17:03 +0200

    edit order by

--- server/database/queries/posts/displayAllPostsQuery.js
+++ server/database/queries/posts/displayAllPostsQuery.js
@@ -1,5 +1,5 @@
 const connection = require('../../config/connection');
 
-const displayAllPostsQuery = () => connection.query('SELECT *, p.id FROM posts p JOIN users u ON (u.id = p.user_id) ORDER BY p.id DESC');
+const displayAllPostsQuery = () => connection.query('SELECT *, p.id, u.name FROM posts p JOIN users u ON (u.id = p.user_id) ORDER BY p.vote DESC');
 // u.name, p.title, p.post,p.date
 module.exports = displayAllPostsQuery;
